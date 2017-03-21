6a8f96350988c36773b624eaba6af8bea86f7754
Stephen Edgar <stephen@netweb.com.au>
2017-03-21 21:16:23 +1100

    Puppet coding standards

--- modules/gmagick/manifests/init.pp
+++ modules/gmagick/manifests/init.pp
@@ -1,18 +1,19 @@
+# Gmagick Chassis Extension
 class gmagick (
-	$path = "/vagrant/extensions/gmagick",
-	$php = $gmagick_config[php]
+  $path = '/vagrant/extensions/gmagick',
+  $php = $::gmagick_config[php]
 ) {
 
-	if versioncmp( "${$php}", '5.4') <= 0 {
-		$php_package = 'php5'
-	}
-	else {
-		$php_package = "php$php"
-	}
+  if versioncmp( "${$php}", '5.4') <= 0 {
+    $php_package = 'php5'
+  }
+  else {
+    $php_package = "php${php}"
+  }
 
-	package { "${$php_package}-gmagick":
-		ensure  => latest,
-		require => Package["${$php_package}-fpm"],
-		notify  => Service["${$php_package}-fpm"]
-	}
+  package { "${$php_package}-gmagick":
+    ensure  => latest,
+    require => Package["${$php_package}-fpm"],
+    notify  => Service["${$php_package}-fpm"]
+  }
 }
